38abfb87e9724383c1ffd740eccfc2578d4559ee
Linh Nguyen <linhx.hust@gmail.com>
2026-06-13 12:25:45 +0700

    Fix 1Password Darwin trampoline handling

--- modules/home-manager/darwin/trampoline-apps/default.nix
+++ modules/home-manager/darwin/trampoline-apps/default.nix
@@ -1,7 +1,22 @@
 # Hook home-manager to make a trampoline for each app we install
 # from: https://github.com/nix-community/home-manager/issues/1341#issuecomment-1870352014
 { config, lib, pkgs, ... }:
-with lib; {
+
+with lib;
+let
+  cfg = config.darwin.trampolineApps;
+in {
+  options.darwin.trampolineApps.extraApps = mkOption {
+    type = types.listOf types.str;
+    default = [ ];
+    example = [ "/Applications/1Password.app" ];
+    description = ''
+      Extra macOS application bundles to expose as trampoline apps in
+      ~/Applications/Home Manager Trampolines. This is useful for GUI apps
+      that should not be installed into the Nix store on Darwin.
+    '';
+  };
+
   config = mkIf pkgs.stdenv.hostPlatform.isDarwin {
     # Install MacOS applications to the user Applications folder. Also update Docked applications
     home.extraActivationPath = with pkgs; [ rsync dockutil gawk ];
@@ -9,7 +24,8 @@ with lib; {
       ${builtins.readFile ./lib-bash/trampoline-apps.sh}
       fromDir="$HOME/Applications/Home Manager Apps"
       toDir="$HOME/Applications/Home Manager Trampolines"
-      sync_trampolines "$fromDir" "$toDir"
+      extraApps=(${escapeShellArgs cfg.extraApps})
+      sync_trampolines "$fromDir" "$toDir" "''${extraApps[@]}"
     '';
   };
 }
